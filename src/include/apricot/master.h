95deb0a4b947da4c178281934b7a55d6761ff27a
Remy Boutonnet <remy.boutonnet@gmail.com>
2014-03-27 21:20:26 +0100

    master.h : comments added

--- src/include/apricot/master.h
+++ src/include/apricot/master.h
@@ -4,6 +4,10 @@
 void master_start(int port, char ** argv);
 void master_stop();
 
+/* arguments of the current process 
+ * needed because reincarnate create a new worker
+ * and it is asynchronously executed so we cannot
+ * transmit argv as an argument to reincarnate */
 extern char ** arguments;
 
 #endif
