55d531253f3e363293392902b5df77af14dbf165
Jeremagician <jeremagician@gmail.com>
2014-03-31 20:54:01 +0200

    test http_response_write

--- src/static.c
+++ src/static.c
@@ -1,13 +1,17 @@
 #include <apricot/static.h>
 #include <apricot/csapp.h>
 #include <apricot/http_codes.h>
+#include <apricot/http_header.h>
+#include <string.h>
 
 int static_serve(int clientfd, char * uri)
 {
 	/* THIS IS FOR DEBUG PURPOSE ONLY */
-	char buf1[MAXLINE];
     char body1[MAXBUF], body2[MAXBUF];
-
+	
+	static http_response_t response;
+	bzero(&response, sizeof(response));
+	
     /* Build the HTTP response body */
     snprintf(body1, sizeof(body1), "<html><title>Apricot Test</title>");
     snprintf(body2, sizeof(body2), "%s<body bgcolor=""ffffff"">\r\n", body1);
@@ -16,12 +20,14 @@ int static_serve(int clientfd, char * uri)
     snprintf(body1, sizeof(body1), "%s<hr><em>Apricot Web server</em>\r\n", body2);
 
     /* Print the HTTP response */
-    snprintf(buf1, sizeof(buf1), "HTTP/1.0 %s %s\r\n", "200", "Ok");
-    Rio_writen(clientfd, buf1, strlen(buf1));
-    snprintf(buf1, sizeof(buf1), "Content-type: text/html\r\n");
-    Rio_writen(clientfd, buf1, strlen(buf1));
-    snprintf(buf1, sizeof(buf1), "Content-length: %d\r\n\r\n", (int)strlen(body1));
-    Rio_writen(clientfd, buf1, strlen(buf1));
+	response.http_version_major = 1;
+	response.http_version_minor = 0;
+	response.status_code = HTTP_OK;
+	strcpy(response.reason_phrase, "Ok");
+	strcpy(response.content_type, "text/html");
+	response.content_length = (int)strlen(body1);
+	http_response_write(clientfd, &response);
+	
     Rio_writen(clientfd, body1, strlen(body1));
 	return HTTP_OK;
 }
