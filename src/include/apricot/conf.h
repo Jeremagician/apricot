6332e72cd2c80a0a47164e5c71aea8cc74e63034
Rémy Boutonnet <remy.boutonnet@gmail.com>
2014-03-31 21:34:11 +0200

    added configuration options in conf_t and

--- src/include/apricot/conf.h
+++ src/include/apricot/conf.h
@@ -2,10 +2,17 @@
 #define _CONF_
 
 #define ROOT_MAX 255
+#define NR_HOSTS 100
+#define HOST_NAME_MAX 255
+#define LOG_MAX 255
 
 	typedef struct {
 		int listen_port;
 		char root[ROOT_MAX];
+		char hosts[NR_HOSTS][HOST_NAME_MAX];
+		char hosts_root[NR_HOSTS][HOST_NAME_MAX];
+		int nr_hosts;
+		char log_file[LOG_MAX];
 	} conf_t;
 	
 	/* Lit le fichier de configuration */
--- src/include/apricot/utils.h
+++ src/include/apricot/utils.h
@@ -23,6 +23,6 @@ char * straddr(struct sockaddr_in addr);
 
 /* get client host name */
 
-char * get_client_hostname(int fd);
+char * get_client_hostname(struct sockaddr_in addr);
 
 #endif
